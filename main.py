7cda62527e03ea015721ee361b2bb0edc2c945ee
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2024-04-07 16:27:01 +0300

    yygufu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageFilter
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PyQt5.QtGui import QPixmap
 from ui import Ui_MainWindow
 
 import os
@@ -14,16 +16,22 @@ class Widget(QMainWindow):
         self.filename = None
         self.image = None
         self.ui.folder.clicked.connect(self.choose_folder)
+        self.ui.listWidget.currentRowChanged.connect(self.show_chosen_image)
+        self.ui.black_white_btn.clicked.connect(self.do_black_white)
+        self.ui.sharpness.clicked.connect(self.do_blur_white)
+
 
     def show_image_list(self):
-        self.filename = os.listdir(self.workdir)
-        image = []
+        self.filenames = os.listdir(self.workdir)
+        images = []
         for filename in self.filenames:
-            if filename.endswith(". png") or filename.endswith(". jpg") or filename.endswith(". jpeg"):
+            if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
                 images.append(filename)
 
-        self.ui.image_list.clear()
-        self.ui.image_list.addItems(images)
+        self.ui.listWidget.clear()
+        self.ui.listWidget.addItems(images)
+
+
 
     def choose_folder(self):
         try:
@@ -33,6 +41,49 @@ class Widget(QMainWindow):
             message = QMessageBox()
             message.setText("Помилка відкриття папки!")
             message.exec_()
+
+
+    def load_image(self, imagename):
+        self.image_name = imagename
+        self.image_path = os.path.join(self.workdir, self.image_name)
+        self.image = Image.open(self.image_path)
+
+    def show_image(self):
+        self.ui.label.hide()
+        h = self.ui.label.height()
+        w = self.ui.label.width()
+
+        pixmap_image = QPixmap(self.image_path)
+        pixmap_image = pixmap_image.scaled(w,h,Qt.KeepAspectRatio)
+        self.ui.label.setPixmap(pixmap_image)
+        self.ui.label.show()
+
+
+    def show_chosen_image(self):
+        if self.ui.listWidget.currentRow()>=0:
+            self.image_name = self.ui.listWidget.currentItem().text()
+            self.load_image(self.image_name)
+            self.show_image()
+
+    def save_image(self):
+
+        path = os.path.join(self.workdir, "edited")
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        self.new_path = os.path.join(path, self.image_name)
+        self.new_image.save(self.new_path)
+        self.image_path = self.new_path
+
+    def do_black_white(self):
+        self.new_image = self.image.convert('L')
+        self.save_image()
+        self.show_image()
+
+    def do_blur_white(self):
+        self.new_image = self.image.filter(ImageFilter.BoxBlur(4))
+        self.save_image()
+        self.show_image()
+        self.image_path = self.new_image
 
 
 app = QApplication([])
